7e95c59c986f52568fe5813ccdf79e728299c29a
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-07-01 17:38:34 +0200

    Added checkForCharacterRequirement(), Added test hasLowerAndUppercase()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,5 +15,36 @@ public class Main {
             return false;
         }
         return true;
+    }
+    public static boolean checkForNumber(String password){
+        String[] splitString = password.split("");
+        Pattern pattern = Pattern.compile("[0-9]");
+        for (String s : splitString) {
+            Matcher matcher = pattern.matcher(s);
+            boolean matchFound = matcher.find();
+            if(matchFound) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean checkForCharacterRequirement(String password){
+        boolean hasLowercase = false;
+        boolean hasUppercase = false;
+        Pattern lowerPattern = Pattern.compile("[a-z+]");
+        Matcher lowerMatcher = lowerPattern.matcher(password);
+        if(lowerMatcher.find()){
+            hasLowercase = true;
+        }
+        Pattern upperPattern = Pattern.compile("[A-Z+]");
+        Matcher upperMatcher = upperPattern.matcher(password);
+        if(upperMatcher.find()){
+            hasUppercase = true;
+        }
+        if(hasLowercase && hasUppercase){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -14,9 +14,21 @@ class MainTest {
     void pwdLaenge(){
         //WHEN
         //THEN
-        boolean checkLength = Main.passwordLength("Tardis");
+        boolean checkLength = Main.passwordLength("Dr. Donna");
         //RESULT
         assertTrue(checkLength);
+    }
+    @Test
+    void hasNumber(){
+        String[] testString = {"Tardis","Doctor", "11. Regeneration"};
+        boolean checkForNumber = Main.checkForNumber(testString[2]);
+        assertTrue(checkForNumber);
+    }
+    @Test
+    void hasLowerAndUppercase(){
+       String[] testString = {"Tardis","doctor", "11. Regeneration"};
+       boolean charactersMatchRequirements = Main.checkForCharacterRequirement(testString[0]);
+       assertTrue(charactersMatchRequirements);
     }
 
 }
